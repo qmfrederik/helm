ca6d683eb23b881dc9fedeacae58cc19b1c71495
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-28 00:56:50 +0100

    Prototype TillerClient

---
+++ src/Helm.Tests/TillerClientTests.cs
@@ -0,0 +1,26 @@
+﻿using Helm.Helm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Helm.Tests
+{
+    public class TillerClientTests
+    {
+        [Fact]
+        public void ConstructorNullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TillerClient(null));
+        }
+
+        [Fact(Skip = "Live test")]
+        public async Task GetVersionTest()
+        {
+            TillerClient client = new TillerClient();
+            var version = await client.GetVersionAsync();
+            Assert.NotNull(version);
+        }
+    }
+}
---
+++ src/Helm/Helm/TillerClient.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Hapi.Services.Tiller;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static Hapi.Services.Tiller.ReleaseService;
+
+namespace Helm.Helm
+{
+    public class TillerClient
+    {
+        private readonly string target;
+
+        public TillerClient(string target = "127.0.0.1:44134")
+        {
+            this.target = target ?? throw new ArgumentNullException(nameof(target));
+        }
+
+        public async Task<Hapi.Version.Version> GetVersionAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var channel = new Channel(this.target, ChannelCredentials.Insecure);
+            var releaseServiceClient = new ReleaseServiceClient(channel);
+            var version = await releaseServiceClient.GetVersionAsync(new GetVersionRequest(), cancellationToken: cancellationToken);
+            return version.Version;
+        }
+    }
+}
