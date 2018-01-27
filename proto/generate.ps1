e7fe90a886aface21447f0a677644140ebedf470
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-28 00:47:52 +0100

    Use NuGet package to acquire Protobuf and grpc

--- proto/generate.ps1
+++ proto/generate.ps1
@@ -1,6 +1,13 @@
+$protoc = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\windows_x64\protoc.exe"
+$protocInclude = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\"
+$grpc = Join-Path $env:USERPROFILE ".nuget\packages\grpc.tools\1.8.3\tools\windows_x64\grpc_csharp_plugin.exe"
+
+
 $files = Get-ChildItem -Filter *.proto -Recurse . | Resolve-Path -Relative
+
+$google_deps = "Mgoogle/protobuf/timestamp.proto=github.com/golang/protobuf/ptypes/timestamp,Mgoogle/protobuf/any.proto=github.com/golang/protobuf/ptypes/any"
 
 foreach ($file in $files)
 {
-  ..\bin\protoc.exe --csharp_out=..\src\Helm\Hapi $file
+  & $protoc --plugin=protoc-gen-grpc=$($grpc) --grpc_out=..\src\Helm\Hapi --csharp_out=..\src\Helm\Hapi  -I""$($protocInclude)"" -I. $file
 }
