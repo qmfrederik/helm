fa1f2d50e124e6e7233d0b4beed24415a1e0c53f
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-01-28 20:06:03 +0100

    Add StyleCop rules Use latest GRPC generate which correctly identifies code as auto-generated

--- proto/generate.ps1
+++ proto/generate.ps1
@@ -1,6 +1,6 @@
 $protoc = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\windows_x64\protoc.exe"
 $protocInclude = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\"
-$grpc = Join-Path $env:USERPROFILE ".nuget\packages\grpc.tools\1.8.3\tools\windows_x64\grpc_csharp_plugin.exe"
+$grpc = Join-Path $env:USERPROFILE ".nuget\packages\grpc.tools\1.9.0-pre2\tools\windows_x64\grpc_csharp_plugin.exe"
 
 
 $files = Get-ChildItem -Filter *.proto -Recurse . | Resolve-Path -Relative
