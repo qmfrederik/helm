9fe8e6242be16d47ae12dbc8aac22811c7dd566b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-14 17:26:22 +0100

    Fix NuGet path

--- proto/generate.ps1
+++ proto/generate.ps1
@@ -1,15 +1,17 @@
 $runtime = "windows_x64"
 $suffix = ".exe"
+$profile = $env:USERPROFILE
 
 if ($env:OS -ne "Windows_NT")
 {
   $runtime = "linux_x64"
   $suffix = ""
+  $profile = $env:HOME
 }
 
-$protoc = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\$runtime\protoc$suffix"
-$protocInclude = Join-Path $env:USERPROFILE ".nuget\packages\google.protobuf.tools\3.5.1\tools\"
-$grpc = Join-Path $env:USERPROFILE ".nuget\packages\grpc.tools\1.9.0-pre2\tools\$runtime\grpc_csharp_plugin$suffix"
+$protoc = Join-Path $profile ".nuget\packages\google.protobuf.tools\3.5.1\tools\$runtime\protoc$suffix"
+$protocInclude = Join-Path $profile ".nuget\packages\google.protobuf.tools\3.5.1\tools\"
+$grpc = Join-Path $profile ".nuget\packages\grpc.tools\1.9.0-pre2\tools\$runtime\grpc_csharp_plugin$suffix"
 
 $files = Get-ChildItem -Filter *.proto -Recurse . | Resolve-Path -Relative
 
