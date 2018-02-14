fe6562a6924a823de77938a97258acb9f24031e1
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-14 11:41:03 +0100

    Initialize a Minikube cluster and install helm

---
+++ minikube/install.sh
@@ -0,0 +1,28 @@
+# Install kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.9.3/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Install miniube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.0/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+
+# Install Helm
+curl -sL https://kubernetes-helm.storage.googleapis.com/helm-v2.8.1-linux-amd64.tar.gz -o helm-v2.8.1-linux-amd64.tar.gz
+mkdir ~/helm
+tar xf helm-v2.8.1-linux-amd64.tar.gz -C ~/helm/
+mv ~/helm/linux-amd64/helm /usr/local/bin/helm
+rm -rf ~/helm
+
+# Create the minikube cluster
+sudo minikube start --vm-driver=none --kubernetes-version=v1.9.3 --extra-config=apiserver.Authorization.Mode=RBAC
+minikube update-context
+
+# Wait for the cluster to be ready
+JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
+  until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
+
+# Initialize helm, with RBAC permissions
+kubectl create -f ~/minikube/helm-rbac.yaml
+helm init --service-account tiller
