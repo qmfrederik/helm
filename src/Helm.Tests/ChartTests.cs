8ff77911d9a59d8b001131cfc2ba4e644b259b90
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-15 17:15:08 +0100

    Support reading a chart

---
+++ src/Helm.Tests/ChartTests.cs
@@ -0,0 +1,18 @@
+﻿using Helm.Charts;
+using System.IO;
+using Xunit;
+
+namespace Helm.Tests
+{
+    public class ChartTests
+    {
+        [Fact]
+        public static void OpenTest()
+        {
+            using (Stream stream = File.OpenRead("charts/hello-world-0.1.0.tgz"))
+            {
+                var chart = Chart.Open(stream);
+            }
+        }
+    }
+}
---
+++ src/Helm/Charts/Chart.cs
@@ -0,0 +1,45 @@
+﻿using SharpCompress.Readers;
+using SharpCompress.Readers.Tar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Helm.Charts
+{
+    public class Chart
+    {
+        private Stream stream;
+        private Stream gzipStream;
+        private TarReader reader;
+
+        private Chart(Stream stream)
+        {
+            this.stream = stream ?? throw new ArgumentNullException(nameof(stream));
+            this.gzipStream = new GZipStream(this.stream, CompressionMode.Decompress);
+
+            this.reader = TarReader.Open(this.gzipStream);
+
+            while (this.reader.MoveToNextEntry())
+            {
+                if (!this.reader.Entry.IsDirectory && this.reader.Entry.Key.EndsWith("Chart.yaml"))
+                {
+                    using (Stream entryStream = this.reader.OpenEntryStream())
+                    using (TextReader reader = new StreamReader(entryStream))
+                    {
+                        var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention(), ignoreUnmatched: true);
+                        var metadata = deserializer.Deserialize<Hapi.Chart.Metadata>(reader);
+                    }
+                }
+            }
+        }
+
+        public static Chart Open(Stream stream)
+        {
+            return new Chart(stream);
+        }
+    }
+}
