d08e7d335fd791e90bbd2624b6c4e2a59de43fc5
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-16 00:08:10 +0100

    Add chart tests

--- src/Helm.Tests/ChartTests.cs
+++ src/Helm.Tests/ChartTests.cs
@@ -12,6 +12,23 @@ namespace Helm.Tests
             using (Stream stream = File.OpenRead("charts/hello-world-0.1.0.tgz"))
             {
                 var chart = Chart.Open(stream);
+                Assert.NotNull(chart);
+            }
+        }
+
+        [Fact]
+        public static void MetadataTest()
+        {
+            using (Stream stream = File.OpenRead("charts/hello-world-0.1.0.tgz"))
+            {
+                var chart = Chart.Open(stream);
+                var metadata = chart.Metadata;
+
+                Assert.NotNull(metadata);
+                Assert.Equal("v1", metadata.ApiVersion);
+                Assert.Equal("A Helm chart for Kubernetes", metadata.Description);
+                Assert.Equal("hello-world", metadata.Name);
+                Assert.Equal("0.1.0", metadata.Version);
             }
         }
     }
