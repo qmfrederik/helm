8c1ce9d2e86dd5737433aa96b94b864ff935d885
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-02-16 18:45:06 +0100

    Make TillerLocator easy to use (#5)
    * Make TillerLocator easy to use

--- src/Helm/Helm/TillerLocator.cs
+++ src/Helm/Helm/TillerLocator.cs
@@ -1,6 +1,9 @@
 ﻿using k8s;
 using System;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Helm.Helm
@@ -9,14 +12,19 @@ namespace Helm.Helm
     {
         private readonly IKubernetes kubernetes;
 
+        public TillerLocator(KubernetesClientConfiguration configuration)
+            : this(new Kubernetes(configuration))
+        {
+        }
+
         public TillerLocator(IKubernetes kubernetes)
         {
             this.kubernetes = kubernetes ?? throw new ArgumentNullException(nameof(kubernetes));
         }
 
-        public async Task<IPEndPoint> Locate(string @namespace = "kube-system")
+        public async Task<IPEndPoint> Locate(string @namespace = "kube-system", CancellationToken cancellationToken = default(CancellationToken))
         {
-            var tillerPods = await this.kubernetes.ListNamespacedPodAsync(@namespace, labelSelector: "app=helm,name=tiller").ConfigureAwait(false);
+            var tillerPods = await this.kubernetes.ListNamespacedPodAsync(@namespace, labelSelector: "app=helm,name=tiller", cancellationToken: cancellationToken).ConfigureAwait(false);
 
             if (tillerPods.Items.Count == 0)
             {
@@ -25,6 +33,21 @@ namespace Helm.Helm
 
             var tillerPod = tillerPods.Items[0];
             return new IPEndPoint(IPAddress.Parse(tillerPod.Status.PodIP), 44134);
+        }
+
+        public async Task<Stream> Connect(string @namespace = "kube-system", CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var endPoint = await this.Locate(@namespace, cancellationToken).ConfigureAwait(false);
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await socket.ConnectAsync(endPoint).ConfigureAwait(false);
+            return new NetworkStream(socket, ownsSocket: true);
+        }
+
+        public static Task<Stream> Connect(KubernetesClientConfiguration configuration, string @namespace = "kube-system", CancellationToken cancellationToken = default(CancellationToken))
+        {
+            TillerLocator locator = new TillerLocator(configuration);
+            return locator.Connect(@namespace, cancellationToken);
         }
     }
 }
