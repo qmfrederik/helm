5b6084a043215e2b4e63def0ecd59799b67adf23
Patrick Pun <panyang1217@hotmail.com>
2018-10-31 15:47:32 +0800

    1. Support load manual depedency in "charts" folder 2. Refactored Serialize() 3. Add a static method named LoadFrom

---
+++ src/Helm/Charts/Requirement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helm.Charts
+{
+    internal class Requirement
+    {
+        internal class Dependency
+        {
+            public string Name { get; set; }
+            public string Version { get; set; }
+            public string Repository { get; set; }
+            public string Condition { get; set; }
+        }
+
+        public List<Dependency> Dependencies { get; set; }
+    }
+}
