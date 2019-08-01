d2615f4a7089cf4ddbcee24125695628f997aa65
Daniel Hartl <daniel.hartl@rms.com>
2019-08-01 11:42:19 -0700

    Add logs for integration tests

--- src/Helm.IntegrationTests/TillerLocatorTests.cs
+++ src/Helm.IntegrationTests/TillerLocatorTests.cs
@@ -2,19 +2,32 @@
 using k8s;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Helm.IntegrationTests
 {
     public class TillerLocatorTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public TillerLocatorTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public async Task LocateTest()
         {
+            _testOutputHelper.WriteLine("Trying to locate Tiller endpoint");
+
             Kubernetes kubernetes = new Kubernetes(TestConfiguration.Configure());
 
             TillerLocator locator = new TillerLocator(kubernetes);
             var endPoint = await locator.Locate().ConfigureAwait(false);
+            
             Assert.NotNull(endPoint);
+
+            _testOutputHelper.WriteLine($"Tiller endpoint located at {endPoint}");
         }
     }
 }
