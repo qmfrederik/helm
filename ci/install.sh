e19b8076a8b17a8e413ce76242e218798cf85303
dhartl-rms <46934978+dhartl-rms@users.noreply.github.com>
2019-08-01 13:54:20 -0700

    Update -y switch

--- ci/install.sh
+++ ci/install.sh
@@ -6,7 +6,7 @@ wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.2
+sudo apt-get install -y dotnet-sdk-2.2
 
 # Install PowerShell
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list > /dev/null
