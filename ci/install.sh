306c91d63f0543dc003aed6bf0043e53fb8bb50c
Daniel Hartl <daniel.hartl@rms.com>
2019-08-01 15:02:39 -0700

    Revert all changes

--- ci/install.sh
+++ ci/install.sh
@@ -2,14 +2,14 @@
 
 # Install .NET Core
 # https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-2.1.400
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk-2.2
+sudo apt-get install dotnet-sdk-2.1
 
 # Install PowerShell
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list > /dev/null
+curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y powershell 
 
--- src/Helm.IntegrationTests/TillerLocatorTests.cs
+++ src/Helm.IntegrationTests/TillerLocatorTests.cs
@@ -1,49 +1,20 @@
-﻿using System;
-using Helm.Helm;
+﻿using Helm.Helm;
 using k8s;
 using System.Threading.Tasks;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Helm.IntegrationTests
 {
-    public class ConsoleOutputHelper : ITestOutputHelper
-    {
-        public void WriteLine(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public void WriteLine(string format, params object[] args)
-        {
-            Console.WriteLine(format, args);
-        }
-    }
-
     public class TillerLocatorTests
     {
-        private readonly ITestOutputHelper _testOutputHelper;
-
-        public TillerLocatorTests(ITestOutputHelper testOutputHelper)
-        {
-            _testOutputHelper = new ConsoleOutputHelper();
-        }
-
         [Fact]
         public async Task LocateTest()
         {
-            _testOutputHelper.WriteLine("Trying to locate Tiller endpoint");
-
             Kubernetes kubernetes = new Kubernetes(TestConfiguration.Configure());
-            var nodes = await kubernetes.ListNodeAsync();
-            _testOutputHelper.WriteLine($"{nodes.Items.Count} found");
 
             TillerLocator locator = new TillerLocator(kubernetes);
             var endPoint = await locator.Locate().ConfigureAwait(false);
-            
             Assert.NotNull(endPoint);
-
-            _testOutputHelper.WriteLine($"Tiller endpoint located at {endPoint}");
         }
     }
 }
